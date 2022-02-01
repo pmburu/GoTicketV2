fa5bc603e523b331dd45addfc1ed15029c3e0eb2
Peter Mburu <6551883+pmburu@users.noreply.github.com>
2022-02-01 22:46:10 +0100

    Update local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="de6vf8jh1WfYm00CSifmm3hPsU1hbOWRxeQoxEKxlMYykfh467WDGXrkhwzWILJl",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
