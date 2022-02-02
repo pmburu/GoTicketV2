66f233c8f0d38ae87d9856cd22c37a0f7eb04e1d
VladyslavKT <3677112+Vladislolve@users.noreply.github.com>
2022-02-02 15:24:56 +0100

    Clean the views. Add product page. New favicon and background. Add product url and view. Map postgres db container

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,9 +7,9 @@ from .base import env
 DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="de6vf8jh1WfYm00CSifmm3hPsU1hbOWRxeQoxEKxlMYykfh467WDGXrkhwzWILJl",
-)
+		"DJANGO_SECRET_KEY",
+		default="de6vf8jh1WfYm00CSifmm3hPsU1hbOWRxeQoxEKxlMYykfh467WDGXrkhwzWILJl",
+		)
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
@@ -17,18 +17,18 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "",
-    }
-}
+		"default": {
+			"BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+			"LOCATION": "",
+			}
+		}
 
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = env(
-    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
-)
+		"DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+		)
 
 # WhiteNoise
 # ------------------------------------------------------------------------------
@@ -44,16 +44,16 @@ INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
 DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-    "SHOW_TEMPLATE_CONTEXT": True,
-}
+		"DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+		"SHOW_TEMPLATE_CONTEXT": True,
+		}
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 if env("USE_DOCKER") == "yes":
-    import socket
+	import socket
 
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+	hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+	INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
--- goticket/home/views.py
+++ goticket/home/views.py
@@ -4,22 +4,20 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, "index.html")
 
-
 def about(request):
     return render(request, "about.html", {})
-
 
 def store(request):
     return render(request, "store.html", {})
 
-
 def login(request):
     return render(request, "login.html", {})
-
 
 def signup(request):
     return render(request, "sign_up.html", {})
 
-
 def profile(request):
     return render(request, "profile.html", {})
+
+def product(request):
+	return render(request, "product.html", {})
