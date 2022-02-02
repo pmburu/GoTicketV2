ddef14670cefad512b132e66f3a025dd7bec5897
VladyslavKT <3677112+Vladislolve@users.noreply.github.com>
2022-02-02 00:00:04 +0000

    headers update

--- goticket/home/urls.py
+++ goticket/home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("store/", views.store, name="store"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
+    path("profile/", views.profile , name="profile"),
 ]
